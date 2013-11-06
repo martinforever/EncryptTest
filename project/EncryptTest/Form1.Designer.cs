4e5230d4735dcd23008432dd3022dbfbd7e4223d
Martin <martinforever@163.com>
2013-11-06 19:11:47 +0800

    add several

--- project/EncryptTest/Form1.Designer.cs
+++ project/EncryptTest/Form1.Designer.cs
@@ -28,12 +28,112 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button_Encrypt = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.textBox_Source = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox_Encrypt = new System.Windows.Forms.TextBox();
+            this.textBox_Decrypt = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button_Encrypt
+            // 
+            this.button_Encrypt.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.button_Encrypt.Location = new System.Drawing.Point(315, 471);
+            this.button_Encrypt.Name = "button_Encrypt";
+            this.button_Encrypt.Size = new System.Drawing.Size(75, 23);
+            this.button_Encrypt.TabIndex = 0;
+            this.button_Encrypt.Text = "Encrypt";
+            this.button_Encrypt.UseVisualStyleBackColor = true;
+            this.button_Encrypt.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.textBox_Decrypt, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.textBox_Encrypt, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.textBox_Source, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33555F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33223F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33223F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(717, 408);
+            this.tableLayoutPanel1.TabIndex = 4;
+            // 
+            // textBox_Source
+            // 
+            this.textBox_Source.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox_Source.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox_Source.Location = new System.Drawing.Point(3, 3);
+            this.textBox_Source.Multiline = true;
+            this.textBox_Source.Name = "textBox_Source";
+            this.textBox_Source.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox_Source.Size = new System.Drawing.Size(711, 130);
+            this.textBox_Source.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(315, 510);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Decrypt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // textBox_Encrypt
+            // 
+            this.textBox_Encrypt.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox_Encrypt.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox_Encrypt.Location = new System.Drawing.Point(3, 139);
+            this.textBox_Encrypt.Multiline = true;
+            this.textBox_Encrypt.Name = "textBox_Encrypt";
+            this.textBox_Encrypt.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox_Encrypt.Size = new System.Drawing.Size(711, 129);
+            this.textBox_Encrypt.TabIndex = 4;
+            // 
+            // textBox_Decrypt
+            // 
+            this.textBox_Decrypt.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox_Decrypt.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox_Decrypt.Location = new System.Drawing.Point(3, 274);
+            this.textBox_Decrypt.Multiline = true;
+            this.textBox_Decrypt.Name = "textBox_Decrypt";
+            this.textBox_Decrypt.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox_Decrypt.Size = new System.Drawing.Size(711, 131);
+            this.textBox_Decrypt.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(717, 568);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.button_Encrypt);
+            this.Name = "Form1";
+            this.Text = "EncryptTest";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button_Encrypt;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox textBox_Source;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox_Decrypt;
+        private System.Windows.Forms.TextBox textBox_Encrypt;
     }
 }
 
--- project/EncryptTest/Form1.cs
+++ project/EncryptTest/Form1.cs
@@ -7,14 +7,101 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Security.Cryptography;
 
 namespace EncryptTest
 {
     public partial class Form1 : Form
     {
+        private String filePath = "c:\\";
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Encrypt_Binary();
+            //MD5();
+            //RSA(); 
+            Encrypt_DES();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            //Decrypt_Binary();
+            Decrypt_DES();
+        }
+
+        byte[] key;
+        byte[] iv;
+
+        private void Encrypt_DES()
+        {
+            DESCryptoServiceProvider DesCSP = new DESCryptoServiceProvider();
+            key = DesCSP.Key;
+            iv = DesCSP.IV;
+
+            MemoryStream ms = new MemoryStream();//先创建一个内存流
+            CryptoStream cryStream = new CryptoStream(ms, DesCSP.CreateEncryptor(), CryptoStreamMode.Write);//将内存流连接到加密转换流
+            StreamWriter sw = new StreamWriter(cryStream);
+            sw.WriteLine(textBox_Source.Text);//将要加密的字符串写入加密转换流
+            sw.Close();
+            cryStream.Close();
+            byte[] buffer = ms.ToArray();//将加密后的流转换为字节数组
+            textBox_Encrypt.Text = Convert.ToBase64String(buffer);//将加密后的字节数组转换为字符串
+        }
+
+        private void Decrypt_DES()
+        { 
+            DESCryptoServiceProvider DesCSP = new DESCryptoServiceProvider();
+            DesCSP.Key = key;
+            DesCSP.IV = iv;
+
+            byte[] buffer = Convert.FromBase64String(textBox_Encrypt.Text);
+            MemoryStream ms = new MemoryStream(buffer);//将加密后的字节数据加入内存流中
+            CryptoStream cryStream = new CryptoStream(ms, DesCSP.CreateDecryptor(), CryptoStreamMode.Read);//内存流连接到解密流中
+            StreamReader sr = new StreamReader(cryStream);
+            textBox_Decrypt.Text = sr.ReadToEnd();//将解密流读取为字符串
+            sr.Close();
+            cryStream.Close();
+            ms.Close();
+        }
+
+        private void RSA()
+        {
+            CspParameters param = new CspParameters();
+            param.KeyContainerName = "Olive";//密匙容器的名称，保持加密解密一致才能解密成功
+
+            using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(param))
+            {
+                byte[] plaindata = Encoding.Default.GetBytes(textBox_Source.Text);//将要加密的字符串转换为字节数组
+                byte[] encryptdata = rsa.Encrypt(plaindata, false);//将加密后的字节数据转换为新的加密字节数组
+                textBox_Encrypt.Text = Convert.ToBase64String(encryptdata);//将加密后的字节数组转换为字符串
+            }
+        }
+
+        private void MD5()
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+
+            byte[] palindata = Encoding.Default.GetBytes(textBox_Source.Text);//将要加密的字符串转换为字节数组
+            byte[] encryptdata = md5.ComputeHash(palindata);//将字符串加密后也转换为字符数组
+            textBox_Encrypt.Text = Convert.ToBase64String(encryptdata);//将加密后的字节数组转换为加密字符串
+        }
+
+        private void Encrypt_Binary()
+        {
+            byte[] palindata = Encoding.Default.GetBytes(textBox_Source.Text);
+            textBox_Encrypt.Text = Convert.ToBase64String(palindata);
+        }
+
+        private void Decrypt_Binary()
+        {
+            byte[] palindata = Convert.FromBase64String(textBox_Encrypt.Text);
+            textBox_Decrypt.Text = Encoding.Default.GetString(palindata);
         }
     }
 }
